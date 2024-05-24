d814da2d1ee9ff17ab223538fadcac2b3ceac871
lpigny <lpigny@2D-3IF-DASI-14.vdi.insa-lyon.fr>
2024-05-24 21:31:51 +0200

    js visio eleve avec ajax

--- TP2/src/main/webapp/visioEleve.js
+++ TP2/src/main/webapp/visioEleve.js
@@ -9,3 +9,37 @@ $('#stars').on('click', '.clic', function(event) {
         $('#starn'+i).css('fill','#FFD700');
     }
 });
+
+$(document).ready( function () {
+    $('#boutonFin').on( 'click', function () { // Fonction appelée lors du clic sur le bouton
+        $('#popupFin').css('display', 'block');
+    });
+});
+
+$(document).ready( function () {
+    $('#boutonContinuer').on( 'click', function () { // Fonction appelée lors du clic sur le bouton
+        
+        console.log("clic sur le bouton de Continuer"); 
+        $('#messageAttente').html("Demande en cours de traitement...");
+        // Appel AJAX
+        $.ajax({
+            url: './ActionServlet',
+            method: 'POST',
+            data: {
+                todo: 'envoiNote',
+                note: note
+            },
+            dataType: 'json'
+        })
+        .done( function () { // Fonction appelée en cas d'appel AJAX réussi
+            window.location.href = "accueilEleve.html";
+        })
+        .fail( function (error) { // Fonction appelée en cas d'erreur lors de l'appel AJAX
+            console.log('Error',error); // LOG dans Console Javascript
+            alert("Erreur lors de l'appel AJAX");
+        })
+        .always( function () { // Fonction toujours appelée
+            
+        });
+    });
+});

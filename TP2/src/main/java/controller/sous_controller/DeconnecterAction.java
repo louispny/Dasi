4671fbd4165a00f2070d6487a3a3f03af0056f21
Louis <louis.pigny@gmail.com>
2024-05-27 16:24:08 +0200

    ça doit aller mieux avec les imports

--- TP2/src/main/java/controller/sous_controller/DeconnecterAction.java
+++ TP2/src/main/java/controller/sous_controller/DeconnecterAction.java
@@ -5,10 +5,10 @@ import javax.servlet.http.HttpSession;
 
 public class DeconnecterAction extends Action {
         @Override
-        public void executer(HttpServletRequest request) {; 
+        public void executer(HttpServletRequest request) {
+            System.out.println("DeconnexionAction"); 
             HttpSession session = request.getSession(true);
             session.invalidate();
-            System.out.println("DeconnexionAction");
             request.setAttribute("deconnexion", true) ;
         }
 }
